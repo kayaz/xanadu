87227ad6db2a74e67cdbb8fe3aa713deee179b6b
Jacek <kontakt@jk-design.pl>
2023-12-04 10:38:26 +0100

    Domki - cennik

--- resources/views/front/parse/slider.blade.php
+++ resources/views/front/parse/slider.blade.php
@@ -3,11 +3,13 @@
     <ul class="list-unstyled mb-0 slider-mini">
         @foreach ($list as $p)
             <li>
+                <a href="{{asset('uploads/gallery/images/'.$p->file) }}" class="swipebox" rel="gallery-1">
                 <picture>
                     <source type="image/webp" srcset="{{asset('uploads/gallery/images/webp/'.$p->file_webp) }}">
                     <source type="image/jpeg" srcset="{{asset('uploads/gallery/images/'.$p->file) }}">
                     <img src="{{asset('uploads/gallery/images/'.$p->file) }}" alt="{{ $p->name }}">
                 </picture>
+                </a>
             </li>
         @endforeach
     </ul>
