5915cde81eb9a98d51aca5adeb65b9ef4bfb7ba9
Jacek <kontakt@jk-design.pl>
2023-12-08 01:22:52 +0100

    Zwierzeta - tytul i naglowek

--- resources/views/front/pool/index.blade.php
+++ resources/views/front/pool/index.blade.php
@@ -15,44 +15,42 @@
             <div class="row">
                 <div class="col-12 text-left">
                     <h2 class="section-title">Basen w ośrodku wczasowym w Sianożętach</h2>
-                    <div class="text-justify">
-                        <p><b>Za dodatkową opłatą</b> do dyspozycji naszych gości, <b>na terenie zaprzyjaźnionego ośrodka wczasowego oddalonego od Xanadu o zaledwie 100 metrów</b>, dostępny jest basen zewnętrzny z podgrzewaną wodą i atrakcjami dla dzieci i dorosłych. Znajdują się tu między innymi:</p>
-                        <p>&nbsp;</p>
-                        <ul class="mb-0">
-                            <li>zjeżdżalnia dwutorowa o długości ośmiu metrów</li>
-                            <li>wylewnica do masażu</li>
-                            <li>przeciwprąd</li>
-                        </ul>
-                        <p>&nbsp;</p>
-                        <p>Na wielkość basenu 20 m x 10 m składa się basen rekreacyjny o głębokości 140 cm oraz brodzik o głębokości 50 cm. W obu częściach woda jest podgrzewana.</p>
-                        <p>&nbsp;</p>
-                        <p>Przy basenie znajduje się także jacuzzi wyposażone w 24 dysze do hydromasażu, również z podgrzewaną wodą. Spędzanie czasu w jacuzzi stanowi doskonałą formę relaksu.</p>
-                    </div>
+                    <p><b>Za dodatkową opłatą</b> do dyspozycji naszych gości, <b>na terenie zaprzyjaźnionego ośrodka wczasowego oddalonego od Xanadu o zaledwie 100 metrów</b>, dostępny jest basen zewnętrzny z podgrzewaną wodą i atrakcjami dla dzieci i dorosłych. Znajdują się tu między innymi:</p>
+                    <p>&nbsp;</p>
+                    <ul class="mb-0">
+                        <li>zjeżdżalnia dwutorowa o długości ośmiu metrów</li>
+                        <li>wylewnica do masażu</li>
+                        <li>przeciwprąd</li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <p>Na wielkość basenu 20 m x 10 m składa się basen rekreacyjny o głębokości 140 cm oraz brodzik o głębokości 50 cm. W obu częściach woda jest podgrzewana.</p>
+                    <p>&nbsp;</p>
+                    <p>Przy basenie znajduje się także jacuzzi wyposażone w 24 dysze do hydromasażu, również z podgrzewaną wodą. Spędzanie czasu w jacuzzi stanowi doskonałą formę relaksu.</p>
                 </div>
             </div>
             <div class="row mt-5 pt-3">
                 <div class="col-12 d-flex justify-content-center">
                     <div class="icon-box icon-box-blue">
                         <div>
-                            <img src="https://xanaduhotel.test/images/icons/icon-houses.png" alt="" width="70" height="70">
+                            <img src="{{ asset('/images/icons/icon-pool.png') }}" alt="" width="70" height="70">
                             <h3>Basen rekreacyjny</h3>
                         </div>
                     </div>
                     <div class="icon-box icon-box-blue">
                         <div>
-                            <img src="https://xanaduhotel.test/images/icons/icon-rooms.png" alt="" width="70" height="70">
+                            <img src="{{ asset('/images/icons/icon-kidspool.png') }}" alt="" width="70" height="70">
                             <h3>Brodzik dla dzieci</h3>
                         </div>
                     </div>
                     <div class="icon-box icon-box-blue">
                         <div>
-                            <img src="https://xanaduhotel.test/images/icons/icon-food.png" alt="" width="70" height="70">
+                            <img src="{{ asset('/images/icons/icon-jacuzzi.png') }}" alt="" width="70" height="70">
                             <h3>Jacuzzi</h3>
                         </div>
                     </div>
                     <div class="icon-box icon-box-blue">
                         <div>
-                            <img src="https://xanaduhotel.test/images/icons/icon-parking.png" alt="" width="70" height="70">
+                            <img src="{{ asset('/images/icons/icon-sauna.png') }}" alt="" width="70" height="70">
                             <h3>Sauna fińska</h3>
                         </div>
                     </div>
@@ -68,19 +66,17 @@
             <div class="row">
                 <div class="col-12 text-left">
                     <h2 class="section-title">Sauna</h2>
-                    <div class="text-justify">
-                        <p>Formą relaksu jest również korzystanie z sauny suchej (fińskiej). Pomieszczenie rozgrzane do temperatury około 95°C, o wilgotności 10%. Tak wysoką temperaturę utrzymuje piec z rozgrzanymi kamieniami. W suchej saunie można przebywać od 8 do 15 minut, po czym należy z niej wyjść, wziąć zimny prysznic, osuszyć się i wrócić do sauny. Po trzecim wyjściu z sauny powinno się natrzeć ciało olejkiem i wypić około litra wody lub soku.</p>
-                        <p>&nbsp;</p>
-                        <p>Sauna fińska:</p>
-                        <ul>
-                            <li>poprawia krążenie</li>
-                            <li>przyśpiesza metabolizm płynów z organizmu</li>
-                            <li>wzmacnia układ odpornościowy</li>
-                            <li>oczyszcza organizm z toksyn</li>
-                        </ul>
-                        <p>&nbsp;</p>
-                        <p><b>Kompleks basenowy i strefa wellness czynne są od 23 maja do 4 września.</b></p>
-                    </div>
+                    <p>Formą relaksu jest również korzystanie z sauny suchej (fińskiej). Pomieszczenie rozgrzane do temperatury około 95°C, o wilgotności 10%. Tak wysoką temperaturę utrzymuje piec z rozgrzanymi kamieniami. W suchej saunie można przebywać od 8 do 15 minut, po czym należy z niej wyjść, wziąć zimny prysznic, osuszyć się i wrócić do sauny. Po trzecim wyjściu z sauny powinno się natrzeć ciało olejkiem i wypić około litra wody lub soku.</p>
+                    <p>&nbsp;</p>
+                    <p>Sauna fińska:</p>
+                    <ul>
+                        <li>poprawia krążenie</li>
+                        <li>przyśpiesza metabolizm płynów z organizmu</li>
+                        <li>wzmacnia układ odpornościowy</li>
+                        <li>oczyszcza organizm z toksyn</li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <p><b>Kompleks basenowy i strefa wellness czynne są od 23 maja do 4 września.</b></p>
                 </div>
             </div>
         </div>
