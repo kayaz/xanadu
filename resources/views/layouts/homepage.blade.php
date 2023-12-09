74427c062e3287799de3257c258e21155c292bd9
Jacek <kontakt@jk-design.pl>
2023-12-09 15:33:41 +0100

    Sniadania

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/styles.min.css?date=0812') }}" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css?date=0912') }}" rel="stylesheet">
 
     @stack('style')
 </head>
