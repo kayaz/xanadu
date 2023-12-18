7a130da43e5000900cbe17f6f4740ee2715dc8c4
Jacek <kontakt@jk-design.pl>
2023-12-19 00:19:29 +0100

    RWD part 5

--- resources/views/front/rooms/index.blade.php
+++ resources/views/front/rooms/index.blade.php
@@ -107,7 +107,7 @@
                     <div class="container mt-3 mt-md-5 p-0">
                         <div class="row">
                             <div class="col-12">
-                                <h2 class="section-title text-center"><span class="text-blue">Galeria</span></h2>
+                                <h2 class="section-title text-center"><span class="text-blue">Wideo</span></h2>
                             </div>
                             <div class="col-12">
                                 <iframe width="560" height="315" src="https://www.youtube.com/embed/hlfIoCWMu6k?si=OykIob814tiYFb8X" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
