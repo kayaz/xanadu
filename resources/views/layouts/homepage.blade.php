5a834668f19348ccd825d34ff90210b00d621b4e
Jacek <kontakt@jk-design.pl>
2023-12-24 02:12:29 +0100

    Galerie w pokojach

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/styles.min.css?date=2412') }}" rel="stylesheet">
+    <link href="{{ asset('/css/styles.min.css?date=24121') }}" rel="stylesheet">
 
     @stack('style')
 </head>
