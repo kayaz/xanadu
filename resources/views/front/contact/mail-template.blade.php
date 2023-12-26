b858da355f782aae544596762c90b6470bb9b720
Jacek <kontakt@jk-design.pl>
2023-12-26 12:30:52 +0100

    Galerie w pokojach

--- resources/views/front/contact/mail-template.blade.php
+++ resources/views/front/contact/mail-template.blade.php
@@ -3,8 +3,8 @@
     <title>{{ config('app.name') }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <style>a{text-decoration: none}table img{display: block;border: 0;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;}table tr{border-collapse: collapse;}table p{margin: 0;-webkit-text-size-adjust: none;-ms-text-size-adjust: none;mso-line-height-rule: exactly;line-height: 190%}</style></head>
-<body style="background-color:#ffffff;padding:0;margin:0">
-<table align="center" border="0" cellpadding="0" cellspacing="0" style="margin:0 auto;width:100%;padding:0;border-collapse:collapse;background-color:#ffffff">
+<body style="background-color:#113f63;padding:0;margin:0">
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="margin:0 auto;width:100%;padding:0;border-collapse:collapse;background-color:#113f63">
     <tr>
         <td style="padding:30px">
             <table align="center" cellpadding="0" cellspacing="0" border="0" width="600" style="margin:0 auto;width:600px;padding:0;border-collapse:collapse">
